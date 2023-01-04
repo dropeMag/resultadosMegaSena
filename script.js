af4bcc8857b0705ffdc635e1bab2becd71039bf8
Pedro Magalhães <107576199+dropeMag@users.noreply.github.com>
2023-01-04 13:39:35 -0300

    commit_update
    Updating:
    - Html, CSS, JavaScript & Design.

--- script.js
+++ script.js
@@ -1,85 +1,104 @@
 import { resultados } from "./dados.js"
 
 // números que mais apareceram
-let numMais1 = document.querySelector('#numMais1')
-let numMais2 = document.querySelector('#numMais2')
+let listaNumeros = document.getElementById('lista-numeros')
 
-// números na média
-let numMedio1 = document.querySelector('#numMedio1')
-let numMedio2 = document.querySelector('#numMedio2')
-
-// números que menos apareceram
-let numMenos1 = document.querySelector('#numMenos1')
-let numMenos2 = document.querySelector('#numMenos2')
-
-// botão
-let btn = document.getElementById('btn')
+let listaJogos = document.getElementById('lista-jogos')
 
 // lista que salva o número e a quantidades de vezes que apareceu
 let lista1 = []
 
+// lista que salva temporariamente os números sorteados
+let lista2 = []
 
 // função que coloca os números na ordem de quem apareceu mais vezes
 function ordenar(a, b){
     return b.qnt - a.qnt;
 }
 
+function ordenar2(a, b){
+    return a - b;
+}
+
+
+function gerarJogos(){
+    let valor;
+
+    for(let escolhaNum = 10; escolhaNum < 61; escolhaNum += 10){
+        do {
+            valor = lista1[Math.floor(Math.random() * escolhaNum)].num
+        } while (lista2.includes(valor));
+    
+        lista2.push(valor)
+    }
+
+    lista2.sort(ordenar2)
+    listaJogos.innerHTML += anotar1()
+    lista2.length = 0
+}
+
+
+
+
+
+
+function anotar1(){
+    return `<ul>
+            <li>${lista2[0]}</li>
+            <li>${lista2[1]}</li>
+            <li>${lista2[2]}</li>
+            <li>${lista2[3]}</li>
+            <li>${lista2[4]}</li>
+            <li>${lista2[5]}</li>
+            </ul>`
+}
+
+
+
+
+
+
+
 // função que coloca no HTML o número e sua quantidade de vezes que apareceu
-function anotar(val){
-    return `<td>
-            <h1>${lista1[val].num}</h1>        
-            <p>${lista1[val].qnt}x</p></td>`
+function anotar2(val){
+    return `<li>
+            <p>${lista1[val].num}</p>        
+            <span>${lista1[val].qnt}x</span>
+        </li>`
 }
 
 // evento principal
-btn.addEventListener('click', ()=>{
-    // faz a contagem dos números
-    for(let j = 1; j <= 60; j++){
-        let cont = 0
-
-        for(let i = 0; i < resultados.length; i++){    
-            if(resultados[i] == j){
-                cont++
+function eventoPrincipal(){
+        // conta a frequência dos números
+        for(let j = 1; j <= 60; j++){
+            let cont = 0
+        
+            for(let i = 0; i < resultados.length; i++){    
+                if(resultados[i] == j){
+                    cont++
+                }
             }
+        
+            let resul = {
+                num: j,
+                qnt: cont
+            }
+        
+            lista1.push(resul)
+        }
+        
+        lista1.sort(ordenar)
+        
+        for(let m = 0; m < 5; m++){
+            gerarJogos()
         }
 
-        let resul = {
-            num: j,
-            qnt: cont
+        // printa os números que mais apareceram
+        for(let k = 0; k < lista1.length; k++){
+            listaNumeros.innerHTML += anotar2(k)
         }
-    
-        lista1.push(resul)
-    }
+}
 
-    lista1.sort(ordenar)
-
-    // printa os números que mais apareceram
-    for(let k = 0; k < 10; k++){
-        numMais1.innerHTML += anotar(k)
-    }
-
-    for(let k = 10; k < 20; k++){
-        numMais2.innerHTML += anotar(k)
-    }
-
-    // printa os números medianos
-    for(let k = 20; k < 30; k++){
-        numMedio1.innerHTML += anotar(k)
-    }
-
-    for(let k = 30; k < 40; k++){
-        numMedio2.innerHTML += anotar(k)
-    }
-
-    // printa os números que menos sairam
-    for(let k = 40; k < 50; k++){
-        numMenos1.innerHTML += anotar(k)
-    }
-
-    for(let k = 50; k < 60; k++){
-        numMenos2.innerHTML += anotar(k)
-    }
-
-    // esconde o botão
-    btn.classList.add('btnUsado')
-})
+for(let l = 0; l < 1; l++){
+    setTimeout(eventoPrincipal, 10)
+}
